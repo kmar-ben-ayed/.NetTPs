f567bc3b3a8c746dbc33f2147fd1e221c0779312
kmarBenAyed <kmarbenayed701@gmail.com>
2026-02-15 14:06:39 +0100

    TP3

---
+++ TP3/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TP3.Models;
+namespace TP3.ViewModels
+{
+    public class CustomerListViewModel
+    {
+        public List<Customer> Customers { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string SearchString { get; set; }
+    }
+}
---
+++ TP3/ViewModels/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using TP3.Models;
+namespace TP3.ViewModels
+{
+    public class MovieVM
+    {
+        public Movie Movie { get; set; } = new Movie();
+        
+        [Display(Name = "Photo")]
+        public IFormFile? Photo { get; set; }
+    }
+}
